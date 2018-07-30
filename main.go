d624eac08304c56cfbc16425f46b3ee9d565114c
TinyKitten <catwalk8205@gmail.com>
2018-07-31 07:13:22 +0900

    新API仕様対応？

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -70,37 +69,33 @@ func main() {
 
 // NearStationResponse StationAPI Response
 type NearStationResponse struct {
-	Addr     string         `json:"addr"`
-	CloseYmd sql.NullString `json:"close_ymd"`
-	ESort    int            `json:"e_sort"`
-	EStatus  int            `json:"e_status"`
-	Gap      float64        `json:"gap"`
-	Lat      float64        `json:"lat"`
-	LineCd   int            `json:"line_cd"`
+	Addr     string  `json:"addr"`
+	CloseYmd string  `json:"close_ymd"`
+	Gap      float64 `json:"gap"`
+	Lat      float64 `json:"lat"`
+	LineCd   int     `json:"line_cd"`
 	Lines    []struct {
-		CompanyCd  int            `json:"company_cd"`
-		ESort      int            `json:"e_sort"`
-		EStatus    int            `json:"e_status"`
-		Lat        float64        `json:"lat"`
-		LineCd     int            `json:"line_cd"`
-		LineColorC string         `json:"line_color_c"`
-		LineColorT sql.NullString `json:"line_color_t"`
-		LineName   string         `json:"line_name"`
-		LineNameH  string         `json:"line_name_h"`
-		LineNameK  string         `json:"line_name_k"`
-		LineType   sql.NullString `json:"line_type"`
-		Lon        float64        `json:"lon"`
-		Zoom       int            `json:"zoom"`
+		CompanyCd  int     `json:"company_cd"`
+		Lat        float64 `json:"lat"`
+		LineCd     int     `json:"line_cd"`
+		LineColorC string  `json:"line_color_c"`
+		LineColorT string  `json:"line_color_t"`
+		LineName   string  `json:"line_name"`
+		LineNameH  string  `json:"line_name_h"`
+		LineNameK  string  `json:"line_name_k"`
+		LineType   string  `json:"line_type"`
+		Lon        float64 `json:"lon"`
+		Zoom       int     `json:"zoom"`
 	} `json:"lines"`
-	Lon          float64        `json:"lon"`
-	OpenYmd      sql.NullString `json:"open_ymd"`
-	Post         string         `json:"post"`
-	PrefCd       int            `json:"pref_cd"`
-	StationCd    int            `json:"station_cd"`
-	StationGCd   int            `json:"station_g_cd"`
-	StationName  string         `json:"station_name"`
-	StationNameK sql.NullString `json:"station_name_k"`
-	StationNameR sql.NullString `json:"station_name_r"`
+	Lon          float64 `json:"lon"`
+	OpenYmd      string  `json:"open_ymd"`
+	Post         string  `json:"post"`
+	PrefCd       int     `json:"pref_cd"`
+	StationCd    int     `json:"station_cd"`
+	StationGCd   int     `json:"station_g_cd"`
+	StationName  string  `json:"station_name"`
+	StationNameK string  `json:"station_name_k"`
+	StationNameR string  `json:"station_name_r"`
 }
 
 func getNearStation(lat float64, lon float64) (station *NearStationResponse, err error) {
