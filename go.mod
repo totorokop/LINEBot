45d2ea453fcc9371220a5227818df64d1c794748
TinyKitten <catwalk8205@gmail.com>
2020-05-08 20:19:50 +0900

    多分gql対応できた

--- go.mod
+++ go.mod
@@ -7,7 +7,9 @@ require (
 	github.com/gin-gonic/gin v1.1.5-0.20170702092826-d459835d2b07
 	github.com/golang/protobuf v1.0.0 // indirect
 	github.com/line/line-bot-sdk-go v1.0.0
+	github.com/machinebox/graphql v0.2.2
 	github.com/mattn/go-isatty v0.0.3 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/ugorji/go v1.1.1 // indirect
 	golang.org/x/net v0.0.0-20180406214816-61147c48b25b // indirect
 	golang.org/x/sys v0.0.0-20180414092825-2281fa97ef7b // indirect
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,6 +11,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/line/line-bot-sdk-go/linebot"
+	"github.com/machinebox/graphql"
 )
 
 func main() {
@@ -39,7 +40,7 @@ func main() {
 			if event.Type == linebot.EventTypeMessage {
 				switch message := event.Message.(type) {
 				case *linebot.LocationMessage:
-					nearStation, err := getNearStation(message.Latitude, message.Longitude)
+					nearStation, err := getClosestStation(message.Latitude, message.Longitude)
 					if err != nil {
 						postMessage := linebot.NewTextMessage("ERROR: " + err.Error())
 						if _, err = bot.ReplyMessage(event.ReplyToken, postMessage).Do(); err != nil {
@@ -47,18 +48,10 @@ func main() {
 						}
 					}
 
-					postMessage := linebot.NewTextMessage("最寄り駅は、" + nearStation.StationName + "駅です。")
+					postMessage := linebot.NewTextMessage("最寄り駅は、" + nearStation.Name + "駅です。")
 					if _, err = bot.ReplyMessage(event.ReplyToken, postMessage).Do(); err != nil {
 						log.Print(err)
 					}
-					/*
-						case *linebot.TextMessage:
-							// source := event.Source
-							postMessage := linebot.NewTextMessage("OK: " + message.Text)
-							if _, err = bot.ReplyMessage(event.ReplyToken, postMessage).Do(); err != nil {
-								log.Print(err)
-							}
-					*/
 				}
 			}
 		}
@@ -67,50 +60,42 @@ func main() {
 	router.Run(":" + port)
 }
 
-// NearStationResponse StationAPI Response
-type NearStationResponse struct {
-	Addr     string  `json:"addr"`
-	CloseYmd string  `json:"close_ymd"`
-	Gap      float64 `json:"gap"`
-	Lat      float64 `json:"lat"`
-	LineCd   int     `json:"line_cd"`
-	Lines    []struct {
-		CompanyCd  int     `json:"company_cd"`
-		Lat        float64 `json:"lat"`
-		LineCd     int     `json:"line_cd"`
-		LineColorC string  `json:"line_color_c"`
-		LineColorT string  `json:"line_color_t"`
-		LineName   string  `json:"line_name"`
-		LineNameH  string  `json:"line_name_h"`
-		LineNameK  string  `json:"line_name_k"`
-		LineType   string  `json:"line_type"`
-		Lon        float64 `json:"lon"`
-		Zoom       int     `json:"zoom"`
-	} `json:"lines"`
-	Lon          float64 `json:"lon"`
-	OpenYmd      string  `json:"open_ymd"`
-	Post         string  `json:"post"`
-	PrefCd       int     `json:"pref_cd"`
-	StationCd    int     `json:"station_cd"`
-	StationGCd   int     `json:"station_g_cd"`
-	StationName  string  `json:"station_name"`
-	StationNameK string  `json:"station_name_k"`
-	StationNameR string  `json:"station_name_r"`
+// ClosestStationResponse is GraphQL query response
+type ClosestStationResponse struct {
+	Address string
+	Name    string
+	Lines   []ClosestStationLine
 }
 
-func getNearStation(lat float64, lon float64) (station *NearStationResponse, err error) {
-	client := &http.Client{}
+// ClosestStationLine is GraphQL query response
+type ClosestStationLine struct {
+	Name string
+}
+
+func getClosestStation(lat float64, lon float64) (station *ClosestStationResponse, err error) {
 	latStr := strconv.FormatFloat(lat, 'f', 10, 64)
 	lonStr := strconv.FormatFloat(lon, 'f', 10, 64)
-	resp, err := client.Get("https://sapi.tinykitten.me/v1/station/near?lat=" + latStr + "&lon=" + lonStr)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	var result NearStationResponse
-	err = json.NewDecoder(resp.Body).Decode(&result)
-	if err != nil {
-		return nil, err
+	client := graphql.NewClient("https://sapi.tinykitten.me/")
+	req := graphql.NewRequest(`
+    query ($latitude: Float!, $longitude: Float!) {
+		stationByCoords(latitude: $latitude, longitude: $longitude) {
+		  name
+		  address
+		  lines {
+			name
+		  }
+		}
+	  }
+	`)
+	req.Var("latitude", latStr)
+	req.Var("longitude", lonStr)
+
+	ctx := context.Background()
+
+	// run it and capture the response
+	var result ClosestStationResponse
+	if err := client.Run(ctx, req, &result); err != nil {
+		log.Fatal(err)
 	}
 
 	return &result, nil
